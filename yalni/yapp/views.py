a9cb5c78d657292ee67a4ba11fe2dd182222e0f9
luke <lukemarak@gmail.com>
2018-06-12 17:31:08 +0530

    second commit

--- yalni/yapp/views.py
+++ yalni/yapp/views.py
@@ -1,6 +1,48 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-
 # Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
+
+@login_required
+def home(request):
+    return render(request, 'home.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+@login_required
+def ipc510(request):
+    return render(request, 'ipc510.html')
+
+@login_required
+def ipc610(request):
+    return render(request, 'ipc610.html')
+
+@login_required
+def acp4000(request):
+    return render(request, 'acp4000.html')
+
+@login_required
+def acp4360(request):
+    return render(request, 'acp4360.html')
+
+@login_required
+def acp2000(request):
+    return render(request, 'acp2000.html')
